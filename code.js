538474c17f7e08be74a0985a79c7c94878ea26be
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-11-07 12:47:50 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,37 @@ function quicksort(array) {
     if (array.length < 1){
         return array;
     }
-    let stack = []
+    let stack = [];
+    let start = 0;
+    let end = array.length -1;
+    stack.push(start);
+    stack.push(end);
+    while(stack.length > 0){
+        stack.pop(start);
+        stack.pop(end);
+        let pivotIn = split(array, start, end);
+        if (pivotIn - 1 > start){
+            stack.push(start, pivotIn-1);
+        }
+        if(pivotIn + 1 < end){
+            stack.push(pivotIn+1, end);
+        }
+
+    }
     
     return array;
+}
+
+function split(arr, low, hi){
+    let pivot, i;
+    if (low>=hi) return;
+    pivot = low;
+    for (i= low +1; i<=hi; i++){
+        if(arr[i]<arr[low]){
+            arr[low]= arr[i];
+        }
+
+    }
+
+
 }
